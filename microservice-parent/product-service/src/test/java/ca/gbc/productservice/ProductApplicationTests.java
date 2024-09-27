db22419c20ff32fa93e25932f0e5db213d12f40a
keyurOd <Keyurod20@gmail.com>
2024-09-27 10:00:39 -0400

    comp3095 - week04 monday lecture

--- microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductApplicationTests.java
+++ microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class ProductServiceApplicationTests {
+class ProductApplicationTests {
 
     @Test
     void contextLoads() {
